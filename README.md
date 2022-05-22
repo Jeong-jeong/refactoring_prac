caa948b0dbaac36c3b475dbc384197be19ba08d1
Noopy <kimm.jeongs@gmail.com>
2022-05-22 23:47:59 +0900

    Feat: 1.1 예시 프로그램 작성

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # refactoring_prac
+
+## Chap_01
+- 알게된 것
+`Intl.NumberFormat`: 언어에 맞는 숫자 서식을 지원하는 객체 생성자.
---
+++ main.js
@@ -0,0 +1,5 @@
+import { statement } from "./chap_01/statement";
+import invoices from "./invoces.json";
+import plays from "./plays.json";
+
+console.log(statement(invoices[0], plays));
