5268dbcbde11f41e6e3782ac59ab39019570a24c
Noopy <kimm.jeongs@gmail.com>
2022-05-24 20:07:54 +0900

    refactor: play 지역 변수 제거

--- chap_01/1.4/statement.js
+++ chap_01/1.4/statement.js
@@ -1,6 +1,6 @@
 import { calcAmount } from "./calcAmount";
 
-export function statement(invoice, plays) {
+export function statement(invoice) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구내역 (고객명: ${invoice.customer})\n`;
@@ -10,21 +10,19 @@ export function statement(invoice, plays) {
     maximumFractionDigits: 2,
   }).format;
 
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-
+  for (let aPerformance of invoice.performances) {
     // @NOTE: switch문을 분리해 calcAmount 함수로 추출
-    const thisAmount = calcAmount({ play, perf });
+    const thisAmount = calcAmount({ aPerformance });
     // 포인트를 적립한다.
-    volumeCredits += Math.max(perf.audience - 30, 0);
+    volumeCredits += Math.max(aPerformance.audience - 30, 0);
 
     // 희극 관객 5명마다 추가 포인트를 제공한다.
     if ("comedy" === play.type) {
-      volumeCredits += Math.floor(perf.audience / 5);
+      volumeCredits += Math.floor(aPerformance.audience / 5);
     }
 
     // 청구 내역을 출력한다.
-    result += `${play.name}: ${format(thisAmount / 100)} ${perf.audience}석\n`;
+    result += `${play.name}: ${format(thisAmount / 100)} ${aPerformance.audience}석\n`;
     totalAmount += thisAmount;
   }
   result += `총액 ${format(totalAmount / 100)}\n`;
